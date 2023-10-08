30745c72291320c09401e4d667ffa6541f4313ab
CarlosRodrigoSchimanski <76493107+CarlosRodrigoSchimanski@users.noreply.github.com>
2023-10-08 19:21:34 -0300

    first week finished
    table from SQLAlquemy, login from flask-login, was hard week.

--- loja/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, Blueprint
-from .user.rotas import usuario_bluprint
-
-app = Flask(__name__)
-
-app.register_blueprint(usuario_bluprint)
--- run.py
+++ run.py
@@ -1,4 +1,9 @@
-from loja import app
+from projeto import create_app, db
+from projeto.models import User
+
+app = create_app()
+'''with app.app_context():
+    db.create_all()'''
 
 if __name__ == "__main__":
     app.run(debug=True)
